39b6d675f3c6699366c6490d46d6ccd72895ed6f
David <david.abraham.1337@gmail.com>
2023-03-15 14:39:28 -0400

    Add script to set keybings on gnome

---
+++ gnome/set_keybindings.sh
@@ -0,0 +1,22 @@
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Alt>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Alt>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Alt>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Alt>4']"
+
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 "['<Alt><Shift>1']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 "['<Alt><Shift>2']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<Alt><Shift>3']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<Alt><Shift>4']"
+
+gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>q']"
+
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Super>Tab']"
+
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+gsettings set org.gnome.mutter dynamic-workspaces false
+#gsettings set org.gnome.shell.keybindings toggle-overview "['<Alt>d']"
+
+#gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "['<Alt>Return']"
